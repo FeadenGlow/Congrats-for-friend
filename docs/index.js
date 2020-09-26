da646042ab164ffaf683a39c147cda3356af9990
Minezor <minezor.github.com>
2020-09-26 16:58:24 +0300

    global update

---
+++ docs/index.js
@@ -0,0 +1,15 @@
+
+function login(){
+    let password = document.getElementById("password");
+    if(password.value == 999999){
+        window.open("next1.html")
+    }
+    if(password.value === "качели"){
+        window.open("next2.html")
+    }
+    if(password.value === "Качели"){
+        window.open("next2.html")
+    }
+}
+
+
